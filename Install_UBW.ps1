2bc08b59d08a7771cda5637614beeb43a117af21
medwards1066 <edwards.mp@gmail.com>
2017-03-20 15:52:45 +0000

    Update Install_UBW.ps1

--- Install_UBW.ps1
+++ Install_UBW.ps1
@@ -2,7 +2,7 @@ $drive = 'E'
 $date = (Get-Date -format yyyyMMddHHmmss)
 
 $cmd = ('"' + $drive + ':\unit4 business world\setup.exe"') 
-$arg = ('/S /V"INSTALLLEVEL=150 INSTALL_IIS=1 /qn /lv*x c:\unit4cloud\ubwinstall_"' + $date + '".log"')
+$arg = ('/S /V"INSTALLLEVEL=150 INSTALL_IIS=1 /qn /lv*x c:\unit4cloud\ubwinstall_' + $date + '.log"')
 
 #write-host $cmd
 #write-host $arg
