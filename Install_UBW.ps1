564e96ffb45a55505db4f04c83028aabfc1f4192
medwards1066 <edwards.mp@gmail.com>
2017-03-20 14:41:35 +0000

    Update Install_UBW.ps1

--- Install_UBW.ps1
+++ Install_UBW.ps1
@@ -1,9 +1,9 @@
 $drive = "E"
 
 $cmd = ('"' + $drive + ':\unit4 business world\setup.exe"') 
-$arg = ('/S /V"INSTALLLEVEL=150 INSTALL_IIS=1 /qn /lv*x c:\temp\abwinstall.txt"')
+$arg = ('/S /V"INSTALLLEVEL=150 INSTALL_IIS=1 /qn /lv*x c:\unit4cloud\ubwinstall.txt"')
 
-write-host $cmd
-write-host $arg
+#write-host $cmd
+#write-host $arg
 
 start-process $cmd -argumentlist $arg
